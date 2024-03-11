0b86982ddfe55e695272484f6de9f8f0f728d23d
msicaja <156959957+msicaja@users.noreply.github.com>
2024-03-11 21:35:27 +0000

    test

--- main.hpp
+++ main.hpp
@@ -16,10 +16,35 @@ int getRandom(void)
         /***************************************************
          * Code your program here
          ***************************************************/
+         return rand() % 100;
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
         /***************************************************
          * Code your program here
          ***************************************************/
+         int avg = (rdnum1 + rdnum2 + rdnum3) / 3;
+         int closest, val1, val2, val3, min;
+
+        val1 = abs(rdnum1 - avg);
+        val2 = abs(rdnum2 - avg);
+        val3 = abs(rdnum3 - avg);
+
+        closest = rdnum1;
+        min = val1;
+
+        if (min > val2) {
+                closest = rdnum2;
+                min = val2;
+        }
+        if (min > val3) {
+                closest = rdnum3;
+                min = val3;
+        }
+        if (min > val1) {
+                closest = rdnum1;
+                min = val1;
+        }
+
+         return closest;
 }
